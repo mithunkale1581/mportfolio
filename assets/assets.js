95a873509dbb57f3de7f5085a9ffcb3fd89dda55
mithunkale1581 <mithunkale1581@gmail.com>
2025-05-07 10:13:57 +0530

    updated about file

--- assets/assets.js
+++ assets/assets.js
@@ -15,7 +15,7 @@ import logo_dark from "./logo_dark.png";
 import mithun_logo from "./mithun_logo.png";
 import mail_icon from "./mail_icon.png";
 import mail_icon_dark from "./mail_icon_dark.png";
-import profile_img from "./profile-img.png";
+import profile_img from "./profile_img.png";
 import download_icon from "./download-icon.png";
 import hand_icon from "./hand-icon.png";
 import header_bg_color from "./header-bg-color.png";
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 "use client";
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Header />
+      <About />
     </>
   );
 }
