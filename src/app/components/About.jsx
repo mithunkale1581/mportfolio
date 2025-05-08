92ef3f82076444211a5a4b0408b385ca9faafc49
mithunkale1581 <mithunkale1581@gmail.com>
2025-05-08 17:16:44 +0530

    updated about component

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,10 +1,38 @@
+import Image from 'next/image';
 import React from 'react'
+import { assets, infoList } from '../../../assets/assets';
 
 const About = () => {
     return (
-        <div>
-            <h4>Introduction</h4>
-            <h2>About me</h2>
+        <div className='w-full px-[12%] py-10 scroll-mt-20' id='about'>
+            <h4 className='text-center mb-2 text-lg font-Ovo'>
+                Introduction</h4>
+            <h2 className='text-center text-5xl font-Ovo'>
+                About me</h2>
+
+            <div className='flex w-full flex-col lg:flex-row gap-20 my-20'>
+                <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+                    <Image src={assets.user_image} alt='user' className='w-full rounded-3xl' />
+                </div>
+                <div className='flex-1'>
+                    <p className='mb-10 max-w-2xl font-Ovo'>
+                        I am a passionate and dedicated software engineer with a strong foundation in web development. I have a keen interest in creating innovative solutions that enhance user experiences and drive business success. My expertise lies in front-end development, where I excel in crafting responsive and visually appealing interfaces using modern technologies. I am always eager to learn and adapt to new challenges, and I thrive in collaborative environments where I can contribute my skills and knowledge to achieve common goals.
+                    </p>
+
+                    <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+                        {
+                            infoList.map(({ icon, iconDark, title, description }, index) => (
+                                <li className='border-[0.5px] border-gray-400 rounded-xl p-6 coursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-[4px_4px_0_#000]'
+                                    key={index}>
+                                    <Image src={icon} alt={title} className='w-7 mt-3' />
+                                    <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
+                                    <p className='text-gray-600 text-sm'>{description}</p>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
