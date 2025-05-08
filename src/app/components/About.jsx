494e254bbabb24bacc7092b9ceabd424fbf6e794
mithunkale1581 <mithunkale1581@gmail.com>
2025-05-08 22:10:13 +0530

    updated about component

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react'
-import { assets, infoList } from '../../../assets/assets';
+import { assets, infoList, toolsData } from '../../../assets/assets';
 
 const About = () => {
     return (
@@ -30,6 +30,16 @@ const About = () => {
                                 </li>
                             ))
                         }
+                    </ul>
+                    <h4 className='my-6 text-gray-700 font-Ovo'>Tools I use</h4>
+                    <ul className='flex items-center gap-3 sm:gap-5'>
+                        {toolsData.map((tool, index) => (
+                            <li className='flex items-center justify-center w-12 
+                            sm:w-14 aspect-square border border-gray-400 rounded-lg curssor-pointer hover:-translate-y-1 duration-500' 
+                            key={index}>
+                                <Image src={tool} alt='Tool' className='w-5 sm:w-7' />
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { assets } from "../../../assets/assets";
 
 const Navbar = () => {
 
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef();
 
   const openMenu = () => {
@@ -14,12 +15,22 @@ const Navbar = () => {
     sideMenuRef.current.style.transform = "translateX(16rem)";
   }
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    })
+  }, []);
+
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image src={assets.header_bg_color} alt="" className="w-full" />
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
         <a href="#top">
           <Image
             alt=""
